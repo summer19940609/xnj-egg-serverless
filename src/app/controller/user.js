75db885c5f332c6268f89b9607a13f3fc195c198
xnj <summer19940609@gmail.com>
2022-01-13 21:20:20 +0800

    ctx

--- src/app/controller/user.js
+++ src/app/controller/user.js
@@ -6,7 +6,6 @@ const { sleep } = require('../utils/index')
 class UserController extends Controller {
   async find() {
     const { ctx } = this
-    const ctx = this.ctx;
     const userName = ctx.query.userName;
     const user = await ctx.service.user.find(userName);
     ctx.body = user;
